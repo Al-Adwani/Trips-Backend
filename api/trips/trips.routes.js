ceced73d04bddbb878166a66341cc8510dda26a3
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-11-22 22:24:39 +0300

    Delete trip Done

--- api/trips/trips.routes.js
+++ api/trips/trips.routes.js
@@ -1,9 +1,24 @@
 const express = require("express");
 const passport = require("passport");
 const upload = require("../../middleware/multer");
-const { fetchTripList, tripCreate } = require("./trips.controllers");
+const {
+  fetchTripList,
+  tripCreate,
+  tripDelete,
+  fetchTrip,
+} = require("./trips.controllers");
 
 const router = express.Router();
+
+router.param("tripId", async (req, res, next, tripId) => {
+  const trip = await fetchTrip(tripId, next);
+  if (trip) {
+    req.trip = trip;
+    next();
+  } else {
+    next({ status: 404, message: "Trip Not Found!" });
+  }
+});
 
 router.get("/", fetchTripList);
 
@@ -12,6 +27,12 @@ router.post(
   passport.authenticate("jwt", { session: false }),
   upload.single("image"),
   tripCreate
+);
+
+router.delete(
+  "/:tripId",
+  passport.authenticate("jwt", { session: false }),
+  tripDelete
 );
 
 module.exports = router;
