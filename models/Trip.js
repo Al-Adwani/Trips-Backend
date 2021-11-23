1e0b10a16431614da91b078af6d1f8e38cc1978c
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-11-23 20:34:13 +0300

    Update Done

--- models/Trip.js
+++ models/Trip.js
@@ -1,5 +1,4 @@
 const { model, Schema } = require("mongoose");
-const mongooseSlugPlugin = require("mongoose-slug-plugin");
 
 const TripSchema = Schema({
   title: { type: String, required: true },
@@ -7,7 +6,5 @@ const TripSchema = Schema({
   image: { type: String },
   owner: { type: Schema.Types.ObjectId, ref: "User" },
 });
-
-TripSchema.plugin(mongooseSlugPlugin, { tmpl: "<%=title%>" });
 
 module.exports = model("Trip", TripSchema);
