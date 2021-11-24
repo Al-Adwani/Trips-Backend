c6d6ef601adee3359fcf6beee3b110f57f760b57
Abdullah Al Adwani <abdullahmmaladwani@gmail.com>
2021-11-24 17:12:28 +0300

    modify

--- models/Profile.js
+++ models/Profile.js
@@ -1,15 +1,12 @@
 const { model, Schema } = require("mongoose");
 
 const ProfileSchema = Schema({
-  image: { type: Schema.Types.ObjectId, ref: "User" },
-
+  user: { type: Schema.Types.ObjectId, ref: "User" },
   bio: { type: String },
+  image: { type: String },
+  /* bio: { type: String },
   //   owner: { type: Schema.Types.ObjectId, ref: "User" },
-  numberOfTrips: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
+   */
 });
 
 module.exports = model("Profile", ProfileSchema);
