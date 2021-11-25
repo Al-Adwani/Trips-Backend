44eb836434c88349513c17476c6cd3cc19471251
Your Name <you@example.com>
2021-11-25 11:30:13 +0300

    profile update and fetch completed!

--- api/profile/profile.controllers.js
+++ api/profile/profile.controllers.js
@@ -1,26 +1,61 @@
 const Profile = require("../../models/Profile");
 
-// exports.fetchProfile = async (req, res, next) => {
-//   try {
-//     const profiles = await Profile.findById();
-//     return profiles;
-//   } catch (error) {
-//     next(error);
-//     console.log(error);
-//   }
-// };
-
-exports.fetchListProfile = async (req, res, next) => {
-
+exports.fetchProfile = async (req, res, next) => {
   try {
-    //populate :نفس سبلنق المودل نبي نعبيه
-    const profile = await Profile.find().populate("user");
-    return res.status(200).json(profile);
+    const { profileId } = req.params;
+    const userProfile = await Profile.findById(profileId);
+    return res.json(userProfile);
   } catch (error) {
     next(error);
     console.log(error);
   }
 };
+
+exports.fetchProfiletoUpdate = async (profileId, next) => {
+  try {
+    const profile = await Profile.findById(profileId);
+    return profile;
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateProfile = async (req, res, next) => {
+  try {
+    console.log(req.profile);
+    if (req.profile.user.toString() === req.user._id.toString()) {
+      if (req.file) {
+        req.body.image = `/${req.file.path}`;
+        req.body.image = req.body.image.replace("\\", "/");
+      }
+      const updatedProfile = await Profile.findByIdAndUpdate(
+        req.profile,
+        req.body
+      );
+      res.status(204).json(updatedProfile);
+    } else {
+      next({ status: 401, message: "You are not the Owner" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+// .populate({
+//   path: "user",
+//   select: [_id, username],
+// });
+
+// exports.fetchListProfile = async (req, res, next) => {
+//   try {
+//     //populate :نفس سبلنق المودل نبي نعبيه
+//     const profile = await Profile.find().populate("user");
+//     return res.status(200).json(profile);
+//   } catch (error) {
+//     next(error);
+//     console.log(error);
+//   }
+// };
 // exports.createProfile = async (res, req, next) => {
 //   try {
 //     req.body.user = req.user._id;
--- api/profile/profile.routes.js
+++ api/profile/profile.routes.js
@@ -1,15 +1,38 @@
 const express = require("express");
-const { fetchListProfile } = require("./profile.controllers");
+const {
+  updateProfile,
+  fetchProfiletoUpdate,
+} = require("./profile.controllers");
 const router = express.Router();
-
-router.get("/userProfile", fetchListProfile);
-
-const { fetchProfile, createProfile } = require("./profile.controllers");
+const { fetchProfile } = require("./profile.controllers");
 const passport = require("passport");
 const upload = require("../../middleware/multer");
 
+// router.get("/userProfile", fetchListProfile);
 
-// router.get("/", fetchProfile);
+router.param("profileId", async (req, res, next, profileId) => {
+  const profile = await fetchProfiletoUpdate(profileId, next);
+  if (profile) {
+    req.profile = profile;
+    next();
+  } else {
+    next({ status: 404, message: "Profile Not Found!" });
+  }
+});
 
+router.post;
+
+router.get(
+  "/:profileId",
+  passport.authenticate("jwt", { session: false }),
+  fetchProfile
+);
+
+router.put(
+  "/:profileId",
+  passport.authenticate("jwt", { session: false }),
+  upload.single("image"),
+  updateProfile
+);
 
 module.exports = router;
